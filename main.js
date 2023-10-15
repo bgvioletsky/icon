2c1bf2a7c6a5dd939c69357141301072e0648430
bgcode <violetcode@icloud.com>
2023-10-15 18:06:03 +0800

    1

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const githubapi = $.require("https://cdn.jsdelivr.net/gh/bgvioletsky/icon/githubapi.js");
-// const githubapi = require("http://192.168.31.134:8081/githubapi.js");
+const method = $.require("https://cdn.jsdelivr.net/gh/bgvioletsky/icon@0.0.3/method.js");
+// const method = require("http://192.168.31.134:8081/method.js");
 
 
 function F() {
@@ -32,7 +32,7 @@ function F() {
                         
                     }
                     d=d.concat(x);
-                    var imgpath = githubapi.getImg(
+                    var imgpath = method.getImg(
                         "/storage/emulated/0/Android/data/com.example.hikerview/files/Documents/bgHouse/icon/src/" +
                           list[eval(getMyVar("mainindex","0"))].dirpath
                       );
@@ -71,11 +71,11 @@ function F() {
                         url:
                           "fileSelect://" +
                           $.toString((list) => {
-                            const githubapi = $.require("https://cdn.jsdelivr.net/gh/bgvioletsky/icon/githubapi.js");
+                            const method = $.require("https://cdn.jsdelivr.net/gh/bgvioletsky/icon/method.js");
                             let dir = "hiker://files/bgHouse/icon/src/"+list[eval(getMyVar("mainindex","0"))].dirpath+"/";
-                            let name = githubapi.getName(input).replace("_fileSelect_", "");
+                            let name = method.getName(input).replace("_fileSelect_", "");
                             let path = dir + name;
-                            if (githubapi.copyFile(input, getPath(path).slice(7), true)) {
+                            if (method.copyFile(input, getPath(path).slice(7), true)) {
                               refreshPage(true);
                               return "toast://添加成功";
                             } else {
@@ -89,7 +89,7 @@ function F() {
                         pic_url: "hiker://files/bgHouse/src/system/19.svg",
                         url: $("", "https|hiker|绝对地址").input((list)=> {
                           let name = input.split("/").at(-1);
-                          const file = $.require("https://cdn.jsdelivr.net/gh/bgvioletsky/icon/githubapi.js");
+                          const file = $.require("https://cdn.jsdelivr.net/gh/bgvioletsky/icon/method.js");
                           let dir = "hiker://files/bgHouse/icon/src/"+list[eval(getMyVar("mainindex","0"))].dirpath+"/"+ name;
                           if (input.match(/http/g)) {
                             downloadFile(input, dir);
@@ -144,16 +144,16 @@ function F() {
                         url:
                           "fileSelect://" +
                           $.toString((list) => {
-                            const githubapi = $.require("https://cdn.jsdelivr.net/gh/bgvioletsky/icon/githubapi.js");
+                            const method = $.require("https://cdn.jsdelivr.net/gh/bgvioletsky/icon/method.js");
                             let dir = "hiker://files/bgHouse/icon/src/"+list[eval(getMyVar("mainindex","0"))].dirpath+"/";
-                            let name = githubapi.getName(input).replace("_fileSelect_", "");
+                            let name = method.getName(input).replace("_fileSelect_", "");
                             let path = dir + name;
-                            githubapi.copyFile(input, getPath(path).slice(7), true);
+                            method.copyFile(input, getPath(path).slice(7), true);
                             let message;
                             if(getItem("token")==""||getItem("repoOwner")==""||getItem("repoName")==""){
                                 return "toast://添加失败请配置API";
                             }else{
-                             message=   githubapi.upload(getItem("token"),getItem("repoOwner"),getItem("repoName"),list[eval(getMyVar("mainindex","0"))].dirpath,name,path)
+                             message=   method.upload(getItem("token"),getItem("repoOwner"),getItem("repoName"),list[eval(getMyVar("mainindex","0"))].dirpath,name,path)
                             }
                             
                             if (message) {
@@ -170,7 +170,7 @@ function F() {
                         pic_url: "hiker://files/bgHouse/src/system/19.svg",
                         url: $("", "https|hiker|绝对地址").input((list)=> {
                           let name = input.split("/").at(-1);
-                          const file = $.require("https://cdn.jsdelivr.net/gh/bgvioletsky/icon/githubapi.js");
+                          const file = $.require("https://cdn.jsdelivr.net/gh/bgvioletsky/icon/method.js");
                           let dir = "hiker://files/bgHouse/icon/src/"+list[eval(getMyVar("mainindex","0"))].dirpath+"/"+ name;
                           if (input.match(/http/g)) {
                             downloadFile(input, dir);
@@ -210,29 +210,48 @@ function F() {
     }
     setResult(d);
 }
-
+//设置页面
 function setting() {
     var d = [];
-    d.push({
-        title: "api设置",
-        url: $("hiker://empty#noRecordHistory##noHistory#").rule(() => {
-            require(config.依赖);
-            api();
-        }),
-        col_type: "text_icon",
-    }, {
+    d.push( {
         title: "仓库设置",
         url: $("hiker://empty#noRecordHistory##noHistory#").rule(() => {
             require(config.依赖);
             warehouse();
         }),
-        col_type: "text_icon",
+        col_type: "icon_small_4",
     }, {
+        title: "api设置",
+        url: $("hiker://empty#noRecordHistory##noHistory#").rule(() => {
+            require(config.依赖);
+            api();
+        }),
+        col_type: "icon_small_4",
+    },{
         title: "文件夹设置",
         url: $("hiker://empty#noRecordHistory##noHistory#").rule(() => {
             require(config.依赖);
             whfiles();
         }),
+        col_type: "icon_small_4",
+    },{
+        title: "文件夹设置",
+        url: $("hiker://empty#noRecordHistory##noHistory#").rule(() => {
+            require(config.依赖);
+            whfiles();
+        }),
+        col_type: "icon_small_4",
+    },{
+        title: "同步仓库",
+        url: $("#noLoading#").lazyRule(() => {
+            if(getItem())
+            getItem("tongbu", "0") == "0"?setItem("tongbu", "1"):setItem("tongbu", "0");
+            refreshPage(true);
+            return "hiker://empty";
+        },api),
+        pic_url:getItem("tongbu", "0") == "1" ?
+        "hiker://files/bgHouse/src/messy/1.svg" :
+        "hiker://files/bgHouse/src/messy/2.svg",
         col_type: "text_icon",
     });
     setResult(d);
@@ -240,10 +259,19 @@ function setting() {
 //用于设置GitHubapi
 function api() {
     var d = [];
+    let api;
+    if(fileExist('hiker://files/bgHouse/icon/json/api.json')){
+        api=JSON.parse(fetch("hiker://files/bgHouse/icon/json/api.json"));
+        setItem("token", api.token);
+        setItem("repoOwner", api.repoOwner);
+        setItem("repoName", api.repoName);
+        //用于读取之前保存的API文件
+    }
+   
     d.push({
         title: getItem("token", "") == "" ? "token:  " : getItem("token", ""),
         col_type: "icon_1_search",
-        url: $(getItem("token", "")).input(() => {
+        url: $(getItem("token", ""),"请输入token").input(() => {
             setItem("token", input);
             refreshPage(true);
         }),
@@ -252,47 +280,53 @@ function api() {
             "repoOwner:  " :
             getItem("repoOwner", ""),
         col_type: "icon_1_search",
-        url: $(getItem("repoOwner", "")).input(() => {
+        url: $(getItem("repoOwner", ""),"请输入repoOwner").input(() => {
             setItem("repoOwner", input);
             refreshPage(true);
         }),
     }, {
         title: getItem("repoName", "") == "" ? "repoName:  " : getItem("repoName", ""),
         col_type: "icon_1_search",
-        url: $(getItem("repoName", "")).input(() => {
+        url: $(getItem("repoName", ""),"请输入repoName").input(() => {
             setItem("repoName", input);
             refreshPage(true);
         }),
     }, {
         title: "测试",
         col_type: "text_3",
-        url: $("README.md").input((githubapi) => {
-            writeFile(`hiker://files/bgHouse/README.md`, "### 谢谢");
-            let message = githubapi.upload(
-                getItem("token", ""),
-                getItem("repoOwner", ""),
-                getItem("repoName", ""),
-                "test",
-                input,
-                "hiker://files/bgHouse/README.md"
-            );
-            deleteFile(`hiker://files/bgHouse/test/README.md`);
-            if (message.name == input) {
+        url: $("#noLoading#").lazyRule((method) => {
+            if(!fileExist("hiker://files/bgHouse/test/README.md")){
+                writeFile(`hiker://files/bgHouse/test/README.md`, "### 这是一个测试文件");
+            }
+            let message= method.query(getItem("repoOwner", ""),getItem("repoName", ""),"test/README.md");
+            if(message.name="README.md"){
+                method.delete_file(getItem("token"),getItem("repoOwner"),getItem("repoName"),message.path,message.sha);
+            }
+            message = method.upload(
+                    getItem("token", ""),
+                    getItem("repoOwner", ""),
+                    getItem("repoName", ""),
+                    "test",
+                    "README.md",
+                    "hiker://files/bgHouse/test/README.md"
+                );
+            
+            if (message.name == "README.md") {
                 return "toast://测试成功";
             } else {
                 return "toast://测试失败请重新修改相关内容";
             }
-        }, githubapi),
+        }, method),
     }, {
         title: "保存",
         url: $("#noLoading#").lazyRule(
             (a, b, c) => {
-                let api = [];
-                api.push({
+                let api={
                     token: a,
                     repoOwner: b,
                     repoName: c,
-                });
+                };
+                writeFile('hiker://files/bgHouse/icon/json/api.json',JSON.stringify(api));
                 return "toast://保存成功";
             },
             getItem("token", ""),
@@ -301,13 +335,14 @@ function api() {
         ),
         col_type: "text_3",
     }, {
-        title: "清除",
-        url: $("#noLoading#").lazyRule(() => {
+        title: "删除",
+        url: $("确定删除api").confirm(() => {
             setItem("token", "");
             setItem("repoOwner", "");
             setItem("repoName", "");
             refreshPage(true);
-            return "toast://清除成功";
+            deleteFile('hiker://files/bgHouse/icon/json/api.json');
+            return "toast://api已删除";
         }),
         col_type: "text_3",
     });
@@ -377,7 +412,7 @@ function whfiles() {
         }, {
             title: "保存",
             col_type: "text_3",
-            url: $().lazyRule((githubapi) => {
+            url: $().lazyRule((method) => {
                 let data = [];
                 if (fileExist("hiker://files/bgHouse/icon/json/localStorage.json")) {
                     data = require("hiker://files/bgHouse/icon/json/localStorage.json");
@@ -387,7 +422,7 @@ function whfiles() {
                     getMyVar("dirpath") != "" &&
                     getMyVar("dirtype") != ""
                 ) {
-                    if (githubapi.test_repeat(data, getMyVar("dirpath"))) {
+                    if (method.test_repeat(data, getMyVar("dirpath"))) {
                         for (let item of data) {
                             if (item.dirpath === getMyVar("dirpath")) {
                                 const index = data.indexOf(item);
@@ -403,7 +438,7 @@ function whfiles() {
                             "hiker://files/bgHouse/icon/json/localStorage.json",
                             JSON.stringify(data)
                         );
-                        githubapi.createDir(getMyVar("dirpath"));
+                        method.createDir(getMyVar("dirpath"));
                         refreshPage(true);
                         return "toast://修改成功";
                     }
@@ -416,17 +451,17 @@ function whfiles() {
                         "hiker://files/bgHouse/icon/json/localStorage.json",
                         JSON.stringify(data)
                     );
-                    githubapi.createDir(getMyVar("dirpath"));
+                    method.createDir(getMyVar("dirpath"));
                     refreshPage(true);
                     return "toast://保存成功";
                 } else {
                     return "toast://请输入完整";
                 }
-            }, githubapi),
+            }, method),
         }, {
             title: "删除",
             col_type: "text_3",
-            url: $().lazyRule((githubapi) => {
+            url: $().lazyRule((method) => {
                 let data = [];
                 if (fileExist("hiker://files/bgHouse/icon/json/localStorage.json")) {
                     if (getMyVar("dirpath") == "") {
@@ -456,7 +491,7 @@ function whfiles() {
                 } else {
                     return "toast://请保存后再操作";
                 }
-            }, githubapi),
+            }, method),
         }, {
             title: "清除",
             col_type: "text_3",
@@ -532,7 +567,7 @@ function whfiles() {
         }, {
             title: "保存",
             col_type: "text_3",
-            url: $().lazyRule((githubapi) => {
+            url: $().lazyRule((method) => {
                 let data = [];
                 if (fileExist("hiker://files/bgHouse/icon/json/webStorage.json")) {
                     data = require("hiker://files/bgHouse/icon/json/webStorage.json");
@@ -542,7 +577,7 @@ function whfiles() {
                     getMyVar("dirpath") != "" &&
                     getMyVar("dirtype") != ""
                 ) {
-                    if (githubapi.test_repeat(data, getMyVar("dirpath"))) {
+                    if (method.test_repeat(data, getMyVar("dirpath"))) {
                         for (let item of data) {
                             if (item.dirpath === getMyVar("dirpath")) {
                                 const index = data.indexOf(item);
@@ -558,7 +593,7 @@ function whfiles() {
                             "hiker://files/bgHouse/icon/json/webStorage.json",
                             JSON.stringify(data)
                         );
-                        githubapi.createDir(getMyVar("dirpath"));
+                        method.createDir(getMyVar("dirpath"));
                         refreshPage(true);
                         return "toast://修改成功";
                     }
@@ -571,17 +606,17 @@ function whfiles() {
                         "hiker://files/bgHouse/icon/json/webStorage.json",
                         JSON.stringify(data)
                     );
-                    githubapi.createDir(getMyVar("dirpath"));
+                    method.createDir(getMyVar("dirpath"));
                     refreshPage(true);
                     return "toast://保存成功";
                 } else {
                     return "toast://请输入完整";
                 }
-            }, githubapi),
+            }, method),
         }, {
             title: "删除",
             col_type: "text_3",
-            url: $().lazyRule((githubapi) => {
+            url: $().lazyRule((method) => {
                 let data = [];
                 if (fileExist("hiker://files/bgHouse/icon/json/webStorage.json")) {
                     if (getMyVar("dirpath") == "") {
@@ -611,7 +646,7 @@ function whfiles() {
                 } else {
                     return "toast://请保存后再操作";
                 }
-            }, githubapi),
+            }, method),
         }, {
             title: "清除",
             col_type: "text_3",
