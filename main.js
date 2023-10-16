1708f4bf112fb1bf01c44562c70485b6fbc991eb
bgcode <violetcode@icloud.com>
2023-10-17 00:30:11 +0800

    bgcode

--- main.js
+++ main.js
@@ -348,9 +348,9 @@ function warehouse() {
           }
         },
         method,
-        getItem("token"),
-        getItem("repoOwner"),
-        getItem("repoName")
+        getItem("token",""),
+        getItem("repoOwner",""),
+        getItem("repoName","")
       ),
       pic_url:
         getItem("warehouse", "0") == "0"
