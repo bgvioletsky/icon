e74918cc3a60bdc6e403b37600ced51cf1c20f52
bgcode <violetcode@icloud.com>
2023-10-16 11:10:26 +0800

    bgcode

--- s.js
+++ s.js
@@ -1,21 +1,27 @@
 if (!config.依赖) {
     //远程依赖索引文件代理地址列表
-    let requirelist = 'https://cdn.jsdelivr.net/gh/'
+    let requirelist = ['https://cdn.jsdelivr.net/gh/bgvioletsky/icon/','https://raw.githubusercontent.com/bgvioletsky/icon/main/']
     let requirefile = "hiker://files/rules/bgHouse/require.json";
     if (fetch(requirefile)) {
         try {
             eval("requirelist=" + fetch(requirefile) + ";");
+            
         } catch (e) { }
     }
-    
+    for(let i in requirelist){
         try { //远程依赖索引
-            require(requirelist + 'bgvioletsky/icon/require.js', { timeout: 2000 });
+            require(requirelist[i] + 'require.js', { timeout: 2000 });
+            if (relyfile) {
+                break;
+            }
         } catch (e) {
             log(e.message);
         }
-
+       
+    }
     writeFile(requirefile, JSON.stringify(requirelist));
     initConfig({
         依赖: relyfile
     });
+   
 }
